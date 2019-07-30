a592969cbd942d8aabeba1b02b584564b5e0a3bd
dicodingreviewer <dicodingreviewer@gmail.com>
2019-07-30 17:04:03 +0700

    initial project :tada:

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hero Animation Sample"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "asset/dicoding.png",
+              width: 150,
+              height: 150,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: RaisedButton(
+                child: Text("To another screen"),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return AnotherScreen();
+                  }));
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AnotherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Another Screen"),
+      ),
+      body: Center(
+        child: Image.asset(
+          "asset/dicoding.png",
+          width: 150,
+          height: 150,
+        ),
+      ),
+    );
+  }
+}
